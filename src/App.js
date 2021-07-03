0aa8f6ae8027290b0896584441f409b9d5c4d84d
Deepak Verma <deepakv032001@gmail.com>
2021-07-03 14:07:43 +0530

    Upload Files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid g-2 headerFontSize">
+          <a className="navbar-brand logoFont" href="/">
+            LMS
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="about.html">
+                  About Us
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="blog.html">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
