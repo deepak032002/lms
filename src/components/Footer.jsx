2ba7ab7f20c0d8a5172257b44e56f2388e704196
Deepak Verma <deepakv032001@gmail.com>
2021-07-04 10:19:21 +0530

    Seventh Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const footer = () => {
     return (
         <>
-            <div className="container d-flex justify-content-center align-items-center Info">
+            <div className="container d-flex justify-content-center align-items-center Info flex-column">
                 <h2>On Devlopement....</h2>
+                <h3>Deepak Verma</h3>
             </div>
         </>
     )
